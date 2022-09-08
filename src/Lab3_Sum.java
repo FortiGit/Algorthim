83f2c8c2d1fd4555b03b0018f8991b2438a86094
efulala <kibru4204@gmail.com>
2022-09-07 22:33:00 -0500

    Algorithm

---
+++ src/Lab3_Sum.java
@@ -0,0 +1,31 @@
+/** Ephrem Kidane
+ * Lab 3 Question 3
+ * Sum of sorted arrays of 0s and 1s using recursion
+ * Time complexty - O(log n)
+ */
+public class Lab3_Sum {
+    int sum =0;
+    private int sumOfInt(int [] arr, int lower, int upper){
+        if(lower>upper)
+            return 0;
+
+        int mid =(lower+upper)/2;
+
+        if(arr[mid]==1){
+            sum++;
+        }
+        sumOfInt(arr,lower,mid-1);
+        sumOfInt(arr,mid+1,upper);
+
+        return sum;
+    }
+    public int sumOfInt(int [] arr){
+        return sumOfInt(arr,0,arr.length-1);
+    }
+
+    public static void main(String[] args) {
+        int [] y = {0,0,0,0,1,1,1,1,1,1};
+        Lab3_Sum test = new Lab3_Sum();
+        System.out.println(test.sumOfInt(y));
+    }
+}
